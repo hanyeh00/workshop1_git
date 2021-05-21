159796167478d6d0875b7990fd9ba9c838ff23e0
hanyeh00 <email@example.com>
2021-05-21 15:44:21 +0430

    this file count words

--- wordCount.py
+++ wordCount.py
@@ -1,7 +1,12 @@
 text = ('this some is sample some text')
 x=text.split()
 word='this'
+l={}
+for i in set(x):
+  
+  if i in l.keys():
+    l.update({i:x.count(i)})
+  else:
+    l[i]=1
 
-for i in range(len(x)):
-  x[i].upper()
-print(x)
+print(l)
